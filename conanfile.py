61f7d96209fd767eebe01c355f4b9b145abf4ac2
Artyom Tetyukhin <51746822+arttet@users.noreply.github.com>
2021-10-27 23:45:28 +0300

    Upgrade CI using a new Conan profile (#7)
    There are three pipelines such as build, coverage, linters.
    Fix some clang-tidy issues
    Cleanup conanfile.py
    Cleanup CMakeLists.txt
    Update .clang-tidy

--- conanfile.py
+++ conanfile.py
@@ -1,75 +1,64 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from conans import ConanFile, CMake
-from conans.tools import os_info, SystemPackageTool, ChocolateyTool
+from conan.tools.cmake import CMake, CMakeToolchain
+from conans import ConanFile
+from conans.tools import ChocolateyTool, SystemPackageTool, os_info
 
 
-class UtilsConan(ConanFile):
+class ProjectTemplateConan(ConanFile):
     name = "cpp-project-template"
     version = "0.0.1"
-    description = "cpp project template"
-    homepage = "https://github.com/arttet/cpp-project-template"
+
+    # Optional metadata
     license = "MIT"
     author = "Artyom Tetyukhin"
-    settings = "os", "arch", "compiler", "build_type"
+    url = "https://github.com/arttet/cpp-project-template"
+    description = "cpp project template"
+    topics = ("cpp")
 
+    # Binary configuration
+    build_policy = "missing"
+    generators = "CMakeDeps", "CMakeToolchain"
+    settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
+        "sanitizer": [None, "ASan", "TSan", "UBSan"],
+        "coverage": [True, False],
+        "tests": [True, False],
 
         "system_requirements": [True, False],
-
         "clang_tidy": "ANY",
         "cppcheck": "ANY",
         "cpplint": "ANY",
         "iwyu": "ANY",
         "lwyu": [True, False],
-
-        "coverage": [True, False],
-        "tests": [True, False],
     }
-
     default_options = {
         "shared": False,
         "fPIC": True,
+        "sanitizer": None,
+        "coverage": False,
+        "tests": False,
 
-        "system_requirements": True,
-
+        "system_requirements": False,
         "clang_tidy": "",
         "cppcheck": "",
         "cpplint": "",
         "iwyu": "",
         "lwyu": False,
-
-        "coverage": False,
-        "tests": False,
     }
-
-    generators = "cmake", "cmake_find_package"
-    build_policy = "missing"
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-    def configure(self):
-        pass
-
     def requirements(self):
         self.requires("fmt/8.0.1")
         self.requires("ms-gsl/3.1.0")
-
-    def package_id(self):
-        pass
 
     def build_requirements(self):
         if self.options.tests:
             self.build_requires("benchmark/1.5.6", force_host_context=True)
             self.build_requires("gtest/1.11.0", force_host_context=True)
-
-    def build_id(self):
-        pass
 
     def system_requirements(self):
         if not self.options.system_requirements:
@@ -100,36 +89,25 @@ class UtilsConan(ConanFile):
         if packages:
             installer.install_packages(packages)
 
-    def source(self):
-        pass
+    def generate(self):
+        tc = CMakeToolchain(self)
 
-    def imports(self):
-        pass
+        tc.variables["CMAKE_CXX_CLANG_TIDY"] = self.options.clang_tidy
+        tc.variables["CMAKE_CXX_CPPCHECK"] = self.options.cppcheck
+        tc.variables["CMAKE_CXX_CPPLINT"] = self.options.cpplint
+        tc.variables["CMAKE_CXX_INCLUDE_WHAT_YOU_USE"] = self.options.iwyu
+        tc.variables["CMAKE_LINK_WHAT_YOU_USE"] = self.options.lwyu
+
+        tc.variables["WITH_TESTS"] = self.options.tests
+
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
 
         # https://docs.microsoft.com/en-us/visualstudio/msbuild/msbuild-command-line-reference?view=vs-2019
         # cmake.msbuild_verbosity = "normal"
-        # cmake.verbose = True
-
-        cmake.definitions["CMAKE_CXX_CLANG_TIDY"] = self.options.clang_tidy
-        cmake.definitions["CMAKE_CXX_CPPCHECK"] = self.options.cppcheck
-        cmake.definitions["CMAKE_CXX_CPPLINT"] = self.options.cpplint
-        cmake.definitions["CMAKE_CXX_INCLUDE_WHAT_YOU_USE"] = self.options.iwyu
-        cmake.definitions["CMAKE_LINK_WHAT_YOU_USE"] = self.options.lwyu
-
-        cmake.definitions["WITH_COVERAGE"] = self.options.coverage
-        cmake.definitions["WITH_TESTS"] = self.options.tests
+        cmake.verbose = True
 
         cmake.configure()
         cmake.build()
-
-    def package(self):
-        pass
-
-    def package_info(self):
-        pass
-
-    def deploy(self):
-        pass
